ce0eecee576cd654fdba63239cda4f9a2f0efe3b
Brad Chao <brad@brad.tw>
2017-05-19 16:28:37 +0800

    20170519v6

--- brad06.php
+++ brad06.php
@@ -1,16 +1,8 @@
 <?php
-    $score = rand(0,100);
-    echo $score . '<br>';
-    if ($score >= 90){
-        echo 'A';
-    }else if ($score >= 80){
-        echo 'B';
-    }else if ($score >= 70){
-        echo 'C';
-    }else if ($score >= 60){
-        echo 'D';
-    }else{
-        echo 'E';
-    }
-
+$a = 10; $b = 3;
+if (--$a >= 10 && $b++ <=3){
+    echo "OK: {$a} : {$b}";
+}else{
+    echo "XX: {$a} : {$b}";
+}
 
