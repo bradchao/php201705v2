b20891c8cb7450f63532333386065d7cd23521f7
Brad Chao <brad@brad.tw>
2017-05-19 15:31:05 +0800

    20170519v3

--- brad03.php
+++ brad03.php
@@ -3,13 +3,12 @@ if (isset($_GET['xx'])) {
     $x = $_GET['xx'];
     $y = $_GET['yy'];
     $r = $x + $y;
-    //echo "$x + $y = $r";
 }
 ?>
 <form>
-    <input type="text" name="xx" id="x"/>
+    <input type="text" name="xx" id="x" value="<?php echo $x; ?>"/>
     +
-    <input type="text" name="yy" id="y"/>
+    <input type="text" name="yy" id="y" value="<?php echo $y; ?>"/>
     <input type="submit" value="="/>
     <?php
         echo $r;
