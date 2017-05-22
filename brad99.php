306ecb818c1a48dd5a4433427d80faf01dd8cfe3
Brad Chao <brad@brad.tw>
2017-05-22 11:37:58 +0800

    20170522v2

--- brad99.php
+++ brad99.php
@@ -1,11 +1,27 @@
 <table width="100%" border="1">
     <?php
-        $start = 4; $col = 7; $line=5;
+        $start = 3; $col = 5; $line=4;
         for ($row=0; $row<$line; $row++){
             echo '<tr>';
             for ($j=$start; $j<$start+$col; $j++){
                 $newj = $j + $row*$col;
-                echo '<td>';
+
+                if ($j%2==0){
+                    if ($row%2==0){
+                        echo "<td bgcolor='yellow'>";
+                    }else{
+                        echo "<td bgcolor='orange'>";
+                    }
+
+                }else{
+                    if ($row%2!=0){
+                        echo "<td bgcolor='yellow'>";
+                    }else{
+                        echo "<td bgcolor='orange'>";
+                    }
+                }
+
+
                 for ($i=1; $i<=9; $i++){
                     $r = $newj * $i;
                     echo "{$newj} x {$i} = {$r}<br>";
