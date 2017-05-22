46f08e7f8053bc2e8f20950a6681819aba2c0a1e
Brad Chao <brad@brad.tw>
2017-05-22 12:01:04 +0800

    20170522v3

--- brad07.php
+++ brad07.php
@@ -5,3 +5,7 @@ if ($a){
 }else{
     echo 'XX';
 }
+echo '-----<br>';
+$a=1;
+$var = ($a==1)?2:3;
+echo $var;
--- brad99.php
+++ brad99.php
@@ -6,21 +6,8 @@
             for ($j=$start; $j<$start+$col; $j++){
                 $newj = $j + $row*$col;
 
-                if ($j%2==0){
-                    if ($row%2==0){
-                        echo "<td bgcolor='yellow'>";
-                    }else{
-                        echo "<td bgcolor='orange'>";
-                    }
-
-                }else{
-                    if ($row%2!=0){
-                        echo "<td bgcolor='yellow'>";
-                    }else{
-                        echo "<td bgcolor='orange'>";
-                    }
-                }
-
+                echo "<td bgcolor='" .
+                    ((($j+$row)%2==0)?'yellow':'orange') . "'>";
 
                 for ($i=1; $i<=9; $i++){
                     $r = $newj * $i;
