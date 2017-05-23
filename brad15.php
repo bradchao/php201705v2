d51afa30f19592ad0ff54a2803be54f2797ce211
Brad Chao <brad@brad.tw>
2017-05-23 15:23:05 +0800

    20170523v5

--- brad15.php
+++ brad15.php
@@ -2,7 +2,7 @@
     if (isset($_GET['twid'])){
         $twid = $_GET['twid'];
 
-        if (preg_match('/^[A-Z][12][0-9]{8}/', $twid)>0){
+        if (preg_match('/^[A-Z][12][0-9]{8}$/', $twid)>0){
             //
             $n12s = substr($twid, 0, 1);
             //echo $n12s;
@@ -10,6 +10,23 @@
             $n12 = strpos($letters, $n12s) + 10;
             $n1 = (int)($n12 / 10);
             $n2 = $n12 % 10;
+            $n3 = substr($twid, 1, 1);
+            $n4 = substr($twid, 2, 1);
+            $n5 = substr($twid, 3, 1);
+            $n6 = substr($twid, 4, 1);
+            $n7 = substr($twid, 5, 1);
+            $n8 = substr($twid, 6, 1);
+            $n9 = substr($twid, 7, 1);
+            $n10 = substr($twid, 8, 1);
+            $n11 = substr($twid, 9, 1);
+
+            $sum = $n1*1 + $n2*9 + $n3*8 + $n4*7 + $n5*6 +
+                $n6*5 + $n7*4 + $n8*3 + $n9*2 + $n10*1 + $n11*1;
+            if ($sum %10 == 0){
+                echo 'OK';
+            }else{
+                echo 'XX';
+            }
 
 
         }else{
