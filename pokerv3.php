2027f3eeb4a4419b2c9b2de9d4a8fbc7ddffb2ea
Brad Chao <brad@brad.tw>
2017-05-23 10:54:42 +0800

    20170523v2

--- pokerv3.php
+++ pokerv3.php
@@ -14,10 +14,15 @@
 ?>
 <table border="1" width="100%">
     <?php
+        $suit = array("<font color='black' size='5'>&spades;</font>",
+            "<font color='red' size='5'>&hearts;</font>",
+            "<font color='red' size='5'>&diams;</font>",
+            "<font color='black' size='5'>&clubs;</font>");
+        $value = array('A',2,3,4,5,6,7,8,9,10,'J','Q','K');
         foreach ($player as $sb){
             echo '<tr>';
             foreach ($sb as $card){
-                echo "<td>{$card}</td>";
+                echo "<td>{$suit[(int)($card/13)]}{$value[$card%13]}</td>";
             }
             echo '</tr>';
         }
