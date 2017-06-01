f05ea62a0f6e58363b2b61228c16982767da2612
Brad Chao <brad@brad.tw>
2017-06-01 11:06:44 +0800

    20170601v5

--- brad41.php
+++ brad41.php
@@ -1,7 +1,26 @@
 <?php
-    $json = file_get_contents(
-        "http://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx");
-    $root = json_decode($json);
-    foreach ($root as $row){
-        echo "{$row->ID}:{$row->Name}<br>";
-    }
+//    $json = file_get_contents(
+//        "http://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx");
+//    $root = json_decode($json);
+//    foreach ($root as $row){
+//        echo "{$row->ID}:{$row->Name}<br>";
+//    }
+
+    $json = '{
+            "id":"123",
+            "name": "brad",
+            "lang": [
+                {"name":"Java",
+                 "level": "1"
+                },
+                {"name":"PHP",
+                 "level": "2"
+                },
+                {"name":"Android",
+                 "level": "3"
+                },
+                {"name":"iOS",
+                 "level": "4"
+                }
+            ]}';
+    
