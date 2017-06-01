2039f237cb9b120f26f17a83c454d4dbd3b485b7
Brad Chao <brad@brad.tw>
2017-06-01 10:20:51 +0800

    20170601v3

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
 <?php
+include 'sql.php';
+session_start();
+if (!isset($_SESSION['member'])) header("Location: login.php");
+
+$memberObj = $_SESSION['member'];
+
+
+
 ?>
-Hello, World
+Hello, <?php echo $memberObj->realname; ?>
 <hr>
 <a href="logout.php">Logout</a>
 
