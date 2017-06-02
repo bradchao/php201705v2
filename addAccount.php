df06e09a4ae209a637abef9c8557ce07f8a46786
Brad Chao <brad@brad.tw>
2017-06-02 10:49:47 +0800

    20170602v3

--- addAccount.php
+++ addAccount.php
@@ -1,5 +1,7 @@
 <?php
 include 'sqlBrad.php';
+$pdo = new pdo($dsn, $user, $passwd, $opt);
+
 if (!isset($_REQUEST['account']))
     header("Location: addMemberV2.php");
 
@@ -7,5 +9,8 @@ $account = $_REQUEST['account'];
 $passwd =
     password_hash(
         $_REQUEST['passwd'], PASSWORD_DEFAULT);
+$rname = $_REQUEST['rname'];
 $sql = "INSERT INTO member (account,passwd,rname) VALUES (?,?,?)";
-
+$stmt = $pdo->prepare($sql);    // statement SQL
+$stmt->execute([$account,$passwd,$rname]);
+header("Location: loginv2.php");
