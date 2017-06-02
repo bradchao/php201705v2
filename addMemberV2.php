38ffbe6f23b9f09564943c939afaa50095358829
Brad Chao <brad@brad.tw>
2017-06-02 14:35:49 +0800

    20170602v7

--- addMemberV2.php
+++ addMemberV2.php
@@ -11,7 +11,7 @@
     function precheck() {
         var account =
             document.getElementById('account').value;
-        alert(account);
+        //alert(account);
         xmlhttp.onreadystatechange = callBack1;
         xmlhttp.open("GET", "brad52.php?a="+account, true);
         xmlhttp.send();
@@ -19,15 +19,21 @@
 
     function callBack1() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("test1").innerHTML =
-                this.responseText;
+            var result = this.responseText;
+            if (result != 'OK'){
+                document.getElementById('crs').innerHTML = '重複';
+
+            }else{
+                document.getElementById('crs').innerHTML = '很好';
+            }
         }
     }
 
 
 </script>
 <form action="addAccount.php">
-    <input id='account' name="account" onblur="precheck()" /><br>
+    <input id='account' name="account" onblur="precheck()" />
+    <span id="crs"></span><br>
     <input type="password" name="passwd" /><br>
     <input name="rname" /><br>
     <input type="submit" value="add" />
